23e58fc6c449187f29f852c34da7ffc30901ab2d
Holly Hester-Reilly <holly.hesterreilly@gmail.com>
2012-08-30 00:46:04 -0400

    moved to test email again

--- app/mailers/triage_mailer.rb
+++ app/mailers/triage_mailer.rb
@@ -2,6 +2,7 @@ class TriageMailer < ActionMailer::Base
   default :from => "MediaMathProduct@gmail.com"
   def send_triage_mail assignee
 	@assignee = assignee #make assignee a private variable, available to the send_triage_mail view
-	mail :to => "product_team@mediamath.com", :cc => "ppo@mediamath.com, hollyhesterreilly@gmail.com", :subject => "Product Request Triage goes to..."
+	mail :to => "hollyberry@gmail.com", :cc => "hollyhesterreilly@gmail.com", :subject => "Product Request Triage goes to..."
+	#mail :to => "product_team@mediamath.com", :cc => "ppo@mediamath.com, hollyhesterreilly@gmail.com", :subject => "Product Request Triage goes to..."
   end
 end
