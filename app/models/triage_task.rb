6b461fcf8e7a2cd58741076df80c697e7cd7533d
Holly Hester-Reilly <holly.hesterreilly@gmail.com>
2012-09-05 23:25:33 -0400

    updated triage task to check for pics in general pool when assignee has no unused pics

--- app/models/triage_task.rb
+++ app/models/triage_task.rb
@@ -12,15 +12,29 @@ class TriageTask
 		#which picture to show in email. First, see if there are any pictures that haven't been used for the assigned participants
 		assignee_pics = @assignee.pictures.where :uses => 0
 		if assignee_pics.size > 0
-				@selected_picture = assignee_pics[rand(assignee_pics.size)]
-				# increment picture usage count and save selected picture
-				@selected_picture.uses += 1
-				@selected_picture.save
+			@selected_picture = assignee_pics[rand(assignee_pics.size)]
+			# increment picture usage count and save selected picture
+			@selected_picture.uses += 1
+			@selected_picture.save
 				
 		else 
 			# for now, if none associated, don't send a picture
 			# check if there are unused pics in the general pool - ie not assigned to any specific participants, and also where :uses => 0
-			# general_pics = pictures.where "participant_id IS NULL"
+			# create an array
+			general_pics = []
+			# go through each picture
+			Picture.all.each do |p|
+				# checks to see if uses are 0 and if there are no participants associated with the picture
+				if p.uses == 0 && p.participants.blank?
+					# then add p to array general_pics
+					general_pics.push p
+				end
+			end
+			if general_pics.size > 0
+				@selected_picture = general_pics[rand(general_pics.size)]
+				@selected_picture.uses += 1
+				@selected_picture.save
+			end
 		end
 	
 		
