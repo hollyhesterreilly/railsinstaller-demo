0a882396f91f8ce57e5ee176811791c7c5c83eeb
Holly Hester-Reilly <holly.hesterreilly@gmail.com>
2012-09-05 23:44:15 -0400

    updated email to go to product_team and ppo

--- app/mailers/triage_mailer.rb
+++ app/mailers/triage_mailer.rb
@@ -3,6 +3,6 @@ class TriageMailer < ActionMailer::Base
   def send_triage_mail(assignee, selected_picture)
 	@assignee = assignee #make assignee a private variable, available to the send_triage_mail view
 	@selected_picture = selected_picture
-	mail :to => "hollyberry@gmail.com", :cc => "hollyhesterreilly@gmail.com", :subject => "Product Request Triage goes to..."
+	mail :to => "product_team@mediamath.com", :cc => "ppo@mediamath.com, hollyhesterreilly@gmail.com", :subject => "Product Request Triage goes to..."
   end
 end
